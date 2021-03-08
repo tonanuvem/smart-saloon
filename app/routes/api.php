a1465a8264d0fecd7262ecefc40dca2a01f41ef8
João Carvalho <jovtrc@gmail.com>
2021-03-07 22:39:50 -0300

    Update routes

--- app/routes/api.php
+++ app/routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('dogs', 'App\Http\Controllers\api\DogController');
+Route::apiResource('clients', 'App\Http\Controllers\api\ClientController');
+Route::apiResource('employess', 'App\Http\Controllers\api\EmployeeController');
